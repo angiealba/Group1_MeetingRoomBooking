72e5a8090c21f59e89e0a83c7f1dfefbbdbfa4bd
Ronald Dave Lazarte <dave@gmail.com>
2024-11-22 15:53:13 +0800

    fix the admin header showing in the login

--- ASI.Basecode.Data/obj/Debug/net6.0/ASI.Basecode.Data.AssemblyInfo.cs
+++ ASI.Basecode.Data/obj/Debug/net6.0/ASI.Basecode.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASI.Basecode.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+089399803bd970d26a8a6c27acb27c1fe18cd9cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5c3bf8dae69383994e077da66e362e4d670778d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASI.Basecode.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASI.Basecode.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
