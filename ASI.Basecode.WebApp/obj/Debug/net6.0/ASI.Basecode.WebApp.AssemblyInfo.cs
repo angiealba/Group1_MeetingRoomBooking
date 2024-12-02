173243dc82793abba7de2d8067c08bf6e841f9eb
Rey Crocs <reycrocs@gmail.com>
2024-12-02 17:06:07 +0800

    Pagination and Search

--- ASI.Basecode.WebApp/obj/Debug/net6.0/ASI.Basecode.WebApp.AssemblyInfo.cs
+++ ASI.Basecode.WebApp/obj/Debug/net6.0/ASI.Basecode.WebApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASI.Basecode.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c486e1e083542ba787f7719d4d295b36e58c21a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b975902d1cc3a1bf75138c23002f28d5b21c996d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASI.Basecode.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASI.Basecode.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
