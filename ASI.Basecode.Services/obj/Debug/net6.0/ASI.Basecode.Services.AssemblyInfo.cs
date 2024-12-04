a130ef7ca25f2b6f0a46524e94c9348544e44ed6
Dabudav <93633342+Lazarte-Dave@users.noreply.github.com>
2024-12-04 22:31:11 +0800

    toastr appearance and notification ui fix (haven't implement toastr to other views)

--- ASI.Basecode.Services/obj/Debug/net6.0/ASI.Basecode.Services.AssemblyInfo.cs
+++ ASI.Basecode.Services/obj/Debug/net6.0/ASI.Basecode.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASI.Basecode.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b62a7af1b1832ec165f07da85face304c8b5e7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57a1162196ad7f83b8b1ee8573bd0803bf6a6d2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASI.Basecode.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASI.Basecode.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
